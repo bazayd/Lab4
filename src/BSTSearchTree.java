af7d3439c0cc293ac1e649b1349a045a652cc7c3
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-08-02 09:22:32 -0700

    added methods for BST search tree class.

--- src/BSTSearchTree.java
+++ src/BSTSearchTree.java
@@ -1,4 +1,122 @@
+import java.util.function.BinaryOperator;
+
 public class BSTSearchTree {
 
+    class BSTNode {
+
+        // Data attribute & 2 pointer attributes (Left and Right Child)
+        private Currency data;
+
+        private BSTNode leftChild;
+
+        private BSTNode rightChild;
+
+        public BSTNode(Currency data) {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+
+        // Getters and Setters for data, left, and right child.
+        public void setData(Currency data) {
+            this.data = data;
+        }
+
+        public Currency getData() {
+            return data;
+        }
+
+        public void setLeftChild(BSTNode leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public BSTNode getLeftChild() {
+            return leftChild;
+        }
+
+        public void setRightChild(BSTNode rightChild) {
+            this.rightChild = rightChild;
+        }
+
+        public BSTNode getRightChild() {
+            return rightChild;
+        }
+    }
+
+    protected BSTNode root;
+
+    // Constructor of BSTSearchTree
+    public BSTSearchTree() {
+        root = null;
+    }
+
+    public BSTSearchTree(Currency value) {
+        BSTNode root = new BSTNode(value);
+    }
+
+    public void insert(Currency dollar) {
+        root = RecursiveInsert(root, dollar);
+    }
+
+    public BSTNode RecursiveInsert(BSTNode root, Currency dollar) {
+        if (root == null) {
+            return new BSTNode(dollar);
+        }else if (!dollar.isGreater(root.getData())) {
+            BSTNode leftChild = root.getLeftChild();
+            leftChild = RecursiveInsert(root.leftChild , dollar);
+        }else if (dollar.isGreater(root.getData())) {
+            BSTNode rightChild = root.getRightChild();
+            rightChild = RecursiveInsert(root.rightChild , dollar);
+        }
+
+        return root;
+    }
+
+    public void InOrderTraversal(BSTNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        InOrderTraversal(root.getLeftChild());
+        System.out.println(root.getData() + "\n");
+        InOrderTraversal(root.getRightChild());
+    }
+
+    public void PreorderTraversal(BSTNode node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.getData());
+        PreorderTraversal(node.getLeftChild());
+        PreorderTraversal(node.getRightChild());
+    }
+
+    public void PostorderTraversal() {
+
+    }
+
+    public void BreadthFirstTraversal() {
+
+    }
+
+    public Dollar search() {
+        return null;
+    }
+
+    public void printBST(BSTNode root, String BSTString) {
+
+        if (root == null) {
+            return;
+        }
+
+        System.out.println(BSTString + "\n" + root.getData());
+        printBST(root.leftChild, BSTString + "\n");
+        printBST(root.rightChild, BSTString + "\n");
+
+    }
 
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        BSTSearchTree binarySearchTree = new BSTSearchTree();
+
+        binarySearchTree.insert(new Dollar(57.12));
+        binarySearchTree.insert(new Dollar(23.44));
+        binarySearchTree.insert(new Dollar(87.43));
+
+        binarySearchTree.printBST(binarySearchTree.root, "");
+
+        binarySearchTree.InOrderTraversal(binarySearchTree.root);
+
     }
 }
